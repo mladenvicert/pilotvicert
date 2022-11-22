7e2ea1594b643cd33034a0a0dae497a7dc27c04c
Mladen <mladen.rabasovic@vicert.com>
2022-11-22 14:12:25 +0100

    Created project

---
+++ Pilot/Pilot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pilot
+//
+//  Created by Mladen Rabasovic on 22.11.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
